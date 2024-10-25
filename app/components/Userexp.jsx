f7510224860ccab3d52bd8783f8819b41b5e427b
siddhant22bce0682 <156163973+Siddhantbg@users.noreply.github.com>
2024-10-25 13:20:01 +0530

    commit

--- app/components/Userexp.jsx
+++ app/components/Userexp.jsx
@@ -5,85 +5,48 @@ const Userexp = () => {
     <>
     <section className="bg-gradient-to-r  body-font from-black via-green-950 to-black text-white p-4 justify-start">
       <div className="container px-5 py-24 mx-auto">
+      <h1 className='text-2xl'>User Experience</h1>
+      <br />
         <div className="flex flex-wrap -m-4">
           {[
             {
-              category: 'CATEGORY',
-              title: 'Raclette Blueberry Nextious Level',
+              title: '@NickName77',
               description:
-                'Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.',
-              likes: '1.2K',
-              comments: '6',
+                'Excellent service, everything is translated quickly, well done. Never cheated with commissions',
+              likes: '21.09.2021,',
+              comments: '12:31',
             },
             {
-              category: 'CATEGORY',
-              title: 'Ennui Snackwave Thundercats',
+              title: '_crypto_ghost_',
               description:
-                'Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.',
-              likes: '1.2K',
-              comments: '6',
+                'I am very grateful to this exchange for help and support! We decided everything in the best way) I am new to the market for crepes. Yesterday, from one platform, I tried to make a withdrawal to the map, and as it turned out, that platform',
+                likes: '15.08.2021,',
+                comments: '08:42',
             },
             {
-              category: 'CATEGORY',
-              title: 'Selvage Poke Waistcoat Godard',
+              title: 'Satrak',
               description:
-                'Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.',
-              likes: '1.2K',
-              comments: '6',
+                'A cool exchanger, even with the first time antentification, everything is simple, understandable and, most importantly, quickly, money is credited almost instantly, I advise it to everyone.',
+              likes: '21.09.2021,',
+              comments: '18:55',
             },
           ].map((item, index) => (
             <div key={index} className="p-4 lg:w-1/3">
-              <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+              <div className="h-full bg-lime-900 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
                 <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
                   {item.category}
                 </h2>
-                <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                <h1 className="title-font sm:text-2xl text-xl font-medium text-green-600 mb-3">
                   {item.title}
                 </h1>
                 <p className="leading-relaxed mb-3">{item.description}</p>
-                <a className="text-indigo-500 inline-flex items-center">
-                  Learn More
-                  <svg
-                    className="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
+                
                 <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
-                  <span className="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                    <svg
-                      className="w-4 h-4 mr-1"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                      <circle cx="12" cy="12" r="3"></circle>
-                    </svg>
+                  <span className="text-green-600 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 ">
                     {item.likes}
                   </span>
-                  <span className="text-gray-400 inline-flex items-center leading-none text-sm">
-                    <svg
-                      className="w-4 h-4 mr-1"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                    </svg>
+                  <span className="text-green-700 inline-flex items-center leading-none text-sm">
+                  
                     {item.comments}
                   </span>
                 </div>
